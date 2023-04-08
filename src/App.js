216adb2a21fdd1315b1c34f4854f205b6c7aab20
yuvraj <ydsalunke5@gmail.com>
2023-04-08 22:46:09 +0530

    Initial Change

--- src/App.js
+++ src/App.js
@@ -1,35 +1,66 @@
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Element from './components/element';
 
 function App() {
+  const [stack, setStack] = useState([]);
+
+  const handlePush = () => {
+    const value = document.getElementById('push-input').value.trim();
+    if (value !== '') {
+      setStack([...stack, value]);
+      document.getElementById('push-input').value = '';
+    }
+  };
+
+  const handlePop = () => {
+    if (stack.length === 0) {
+      window.alert('Stack is empty');
+      return;
+    }
+    const poppedValue = stack.pop();
+    setStack([...stack]);
+    document.getElementById('pop-input').value = poppedValue;
+  };
+
+  const handleTop = () => {
+    if (stack.length === 0) {
+      window.alert('Stack is empty');
+      return;
+    }
+    const topValue = stack[stack.length - 1];
+    document.getElementById('top-input').value = topValue;
+  };
+
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
       <div className="row">
         <div className="col md-6 stack">
-          <Element />
+          {stack.map((value, index) => (
+            <Element key={index} value={value} />
+          ))}
         </div>
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" onClick={handlePush}>
               PUSH
             </button>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" id="push-input" />
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" onClick={handlePop}>
               POP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" id="pop-input" />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" onClick={handleTop}>
               TOP
             </button>
-            <input type="text" disabled className="form-control" />
+            <input type="text" disabled className="form-control" id="top-input" />
           </div>
         </div>
       </div>
--- src/components/element.js
+++ src/components/element.js
@@ -1,6 +1,7 @@
 import React from 'react';
-function Element() {
-  return <div className="element"></div>;
+
+function Element(props) {
+  return <div className="element">{props.value}</div>;
 }
 
 export default Element;
