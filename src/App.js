4a567053c9a606cc418eb1e0cbbe2f0689ce7dd2
Yuvraj <122032242+Yuvraj-source@users.noreply.github.com>
2023-04-11 12:22:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,71 +1,155 @@
 import './styles/App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+
+import React from 'react';
+
 import Element from './components/element';
 
+ 
+
 function App() {
-  const [stack, setStack] = useState([]);
 
-  const handlePush = () => {
-    const value = document.getElementById('push-input').value.trim();
-    if (value !== '') {
-      setStack([...stack, value]);
-      document.getElementById('push-input').value = '';
-    }
-  };
+ const [value, setValue] = React.useState('');
 
-  const handlePop = () => {
-    if (stack.length === 0) {
-      window.alert('Stack is empty');
-      return;
-    }
-    const poppedValue = stack.pop();
-    setStack([...stack]);
-    document.getElementById('pop-input').value = poppedValue;
-  };
+ const [currpop, setPop] = React.useState('');
 
-  const handleTop = () => {
-    if (stack.length === 0) {
-      window.alert('Stack is empty');
-      return;
-    }
-    const topValue = stack[stack.length - 1];
-    document.getElementById('top-input').value = topValue;
-  };
+ const [currtop, setTop] = React.useState('');
 
-  return (
-    <div className="container">
-      <h1 className="header">Stack</h1>
-      <div className="row">
-        <div className="col md-6 stack">
-          {stack.map((value, index) => (
-            <Element key={index} value={value} />
-          ))}
-        </div>
-        <div className="col md-3 operation">
-          <h2>Operations</h2>
-          <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" onClick={handlePush}>
-              PUSH
-            </button>
-            <input type="text" className="form-control" id="push-input" />
-          </div>
-          <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" onClick={handlePop}>
-              POP
-            </button>
-            <input type="text" disabled className="form-control" id="pop-input" />
-          </div>
-          <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" onClick={handleTop}>
-              TOP
-            </button>
-            <input type="text" disabled className="form-control" id="top-input" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+ const [stack, setStack] = React.useState([]);
+
+ 
+
+ const hanldeChange = (event) => {
+
+   var curr = event.target.value;
+
+   setValue(curr);
+
+ };
+
+ const getTop = () => {
+
+   if (stack.length === 0) {
+
+     alert('Stack is empty');
+
+     return;
+
+   }
+
+   var top = stack[0];
+
+   setTop(top);
+
+ };
+
+ const push = () => {
+
+   if (value === '') {
+
+     alert('Please enter a value');
+
+     return;
+
+   }
+
+   setStack([value, ...stack]);
+
+ };
+
+ const pop = () => {
+
+   if (stack.length === 0) {
+
+     alert('Stack is empty');
+
+     return;
+
+   }
+
+   var top = stack[0];
+
+   var arr = stack.slice();
+
+   arr.shift();
+
+   setStack(arr);
+
+   setPop(top);
+
+ };
+
+ 
+
+ return (
+
+   <div className="container">
+
+     <h1 className="header">Stack</h1>
+
+     <div className="row">
+
+       <div className="col md-6 stack">
+
+         {stack.map((item, key) => (
+
+           <Element key={key} val={item} />
+
+         ))}
+
+       </div>
+
+       <div className="col md-3 operation">
+
+         <h2>Operations</h2>
+
+         <div className="input-group push">
+
+           <button onClick={push} className="btn btn-outline-secondary" type="button" id="button-addon2">
+
+             PUSH
+
+           </button>
+
+           <input type="text" value={value} onChange={hanldeChange} className="form-control" />
+
+         </div>
+
+         <div className="input-group pop">
+
+           <button onClick={pop} className="btn btn-outline-secondary" type="button" id="button-addon2">
+
+             POP
+
+           </button>
+
+           <input type="text" disabled value={currpop} className="form-control" />
+
+         </div>
+
+         <div className="input-group top">
+
+           <button onClick={getTop} className="btn btn-outline-secondary" type="button" id="button-addon2">
+
+             TOP
+
+           </button>
+
+           <input type="text" disabled value={currtop} className="form-control" />
+
+         </div>
+
+       </div>
+
+     </div>
+
+   </div>
+
+ );
+
 }
+
+ 
 
 export default App;
